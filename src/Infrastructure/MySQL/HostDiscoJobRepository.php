b942bbc0e93032b11bcaaed3e4cefc4bdd00c418
Kevin Duret <kduret@centreon.com>
2023-10-18 15:53:08 +0200

    fix(sql): fix duplicate entry for host disco jobs (#8)

--- src/Infrastructure/MySQL/HostDiscoJobRepository.php
+++ src/Infrastructure/MySQL/HostDiscoJobRepository.php
@@ -22,34 +22,35 @@ class HostDiscoJobRepository
 
         $count = $properties[self::PROPERTY_NAME]['count'];
 
-        $pluginPackResult = $this->connection->query('SELECT MAX(name) AS max FROM mod_ppm_pluginpack');
-        $firstPluginPackId = ((int) $pluginPackResult->fetch()['max']) + 1;
+        if ($count === 0) {
+            return $ids;
+        }
 
-        $providerTypeResult = $this->connection->query('SELECT MAX(name) AS max FROM mod_host_disco_provider_type');
-        $firstProviderTypeId = ((int) $providerTypeResult->fetch()['max']) + 1;
+        $pluginPackResult = $this->connection->executeQuery('SELECT MAX(pluginpack_id) AS max FROM mod_ppm_pluginpack');
+        $firstPluginPackId = ((int) $pluginPackResult->fetchAssociative()['max']) + 1;
 
-        $providerResult = $this->connection->query('SELECT MAX(name) AS max FROM mod_host_disco_provider');
-        $firstProviderId = ((int) $providerResult->fetch()['max']) + 1;
+        $providerTypeResult = $this->connection->executeQuery('SELECT MAX(id) AS max FROM mod_host_disco_provider_type');
+        $firstProviderTypeId = ((int) $providerTypeResult->fetchAssociative()['max']) + 1;
 
-        $result = $this->connection->query('SELECT MAX(alias) AS max FROM mod_host_disco_job');
-        $firstId = ((int) $result->fetch()['max']) + 1;
+        $providerResult = $this->connection->executeQuery('SELECT MAX(id) AS max FROM mod_host_disco_provider');
+        $firstProviderId = ((int) $providerResult->fetchAssociative()['max']) + 1;
+
+        $result = $this->connection->executeQuery('SELECT MAX(id) AS max FROM mod_host_disco_job');
+        $firstId = ((int) $result->fetchAssociative()['max']) + 1;
         $maxId = $firstId + $count;
 
-        $baseIconsQuery = 'INSERT INTO mod_ppm_icons (icon_file) VALUES (NULL),';
-        $iconsQuery = rtrim($baseIconsQuery, ',');
-        $this->connection->query($iconsQuery);
+        $iconsQuery = 'INSERT INTO mod_ppm_icons (icon_file) VALUES (NULL)';
+        $this->connection->executeQuery($iconsQuery);
 
-        $basePluginPackQuery = 'INSERT INTO mod_ppm_pluginpack (name, slug, version, status, discovery_category_id, icon) VALUES ("my PP' . $firstPluginPackId . '", "my_pp' . $firstPluginPackId . '", "1.1.1", "0", 1, 1),';
-        $pluginPackQuery = rtrim($basePluginPackQuery, ',');
-        $this->connection->query($pluginPackQuery);
+        $pluginPackQuery = 'INSERT INTO mod_ppm_pluginpack (name, slug, version, status, discovery_category_id, icon) VALUES ("my PP' . $firstPluginPackId . '", "my_pp' . $firstPluginPackId . '", "1.1.1", "0", 1, 1)';
+        $this->connection->executeQuery($pluginPackQuery);
 
-        $baseProviderTypeQuery = 'INSERT INTO mod_host_disco_provider_type (name, encryption_salt) VALUES ("my type' . $firstProviderTypeId . '", "salt' . $firstProviderTypeId . '"),';
-        $providerTypeQuery = rtrim($baseProviderTypeQuery, ',');
-        $this->connection->query($providerTypeQuery);
+        $providerTypeQuery = 'INSERT INTO mod_host_disco_provider_type (name, encryption_salt) VALUES ("my type' . $firstProviderTypeId . '", "salt' . $firstProviderTypeId . '")';
+        $this->connection->executeQuery($providerTypeQuery);
 
         $baseProviderQuery = 'INSERT INTO mod_host_disco_provider (pluginpack_id, name, slug, type_id, need_proxy, command_id, host_template_id) VALUES (' . $firstPluginPackId . ', "my provider' . $firstProviderId . '", "my_provider' . $firstProviderId . '", ' . $firstProviderTypeId . ', false, 1, 2),';
         $providerQuery = rtrim($baseProviderQuery, ',');
-        $this->connection->query($providerQuery);
+        $this->connection->executeQuery($providerQuery);
 
         $baseQuery = 'INSERT INTO mod_host_disco_job ' .
             '(alias, provider_id, execution_mode, analysis_mode, save_mode, status, ' .
@@ -57,7 +58,6 @@ class HostDiscoJobRepository
             'VALUES ';
         $valuesQuery = '';
 
-        $insertCount = 0;
         for ($i = $firstId; $i < $maxId; $i++) {
             $ids[] = $i;
             $valuesQuery .= '(' .
@@ -75,25 +75,25 @@ class HostDiscoJobRepository
         }
 
         $query = rtrim($baseQuery . $valuesQuery, ',');
-        $this->connection->query($query);
+        $this->connection->executeQuery($query);
 
         return $ids;
     }
 
     public function purge()
     {
-        $this->connection->query('TRUNCATE mod_host_disco_job');
-        $this->connection->query('TRUNCATE mod_ppm_pluginpack');
-        $this->connection->query('TRUNCATE mod_host_disco_provider');
-        $this->connection->query('TRUNCATE mod_host_disco_provider_type');
-        $this->connection->query('TRUNCATE mod_ppm_icons');
+        $this->connection->executeQuery('TRUNCATE mod_host_disco_job');
+        $this->connection->executeQuery('TRUNCATE mod_ppm_pluginpack');
+        $this->connection->executeQuery('TRUNCATE mod_host_disco_provider');
+        $this->connection->executeQuery('TRUNCATE mod_host_disco_provider_type');
+        $this->connection->executeQuery('TRUNCATE mod_ppm_icons');
     }
 
     public function isHostDiscoInstalled(): bool
     {
-        $result = $this->connection->query('SELECT id FROM modules_informations WHERE name = "centreon-autodiscovery-server"');
+        $result = $this->connection->executeQuery('SELECT id FROM modules_informations WHERE name = "centreon-autodiscovery-server"');
 
-        if ($result->fetch()) {
+        if ($result->fetchAssociative()) {
             return true;
         }
 
